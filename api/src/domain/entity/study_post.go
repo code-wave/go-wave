1d6e61f1d2d19e19242f3709c8fdc4e1dfbb6482
JungMin <zxaq15@gmail.com>
2021-06-11 22:43:09 +0900

    fix: Add json annotation

--- api/src/domain/entity/study_post.go
+++ api/src/domain/entity/study_post.go
@@ -7,20 +7,20 @@ import (
 type StudyPosts []StudyPost
 
 type StudyPost struct {
-	ID           uint64
-	Title        string // 게시글 제목
-	Topic        string // 프로젝트 주제
-	Content      string
-	TechStackID  uint64
-	NumOfMembers int64
-	IsMento      bool
-	Price        int64  // 1인당 필요한 가격
-	StartDate    string // 프로젝트 시작
-	EndDate      string // 프로젝트 끝
-	UserID       int64
-	IsOnline     bool
-	CreatedAt    string
-	UpdatedAt    string
+	ID           uint64 `json:"id"`
+	Title        string `json:"title"` // 게시글 제목
+	Topic        string `json:"topic"` // 프로젝트 주제
+	Content      string `json:"content"`
+	TechStackID  uint64 `json:"tech_stack_id"`
+	NumOfMembers int64  `json:"num_of_members"`
+	IsMento      bool   `json:"is_mento"`
+	Price        int64  `json:"price"`      // 1인당 필요한 가격
+	StartDate    string `json:"start_date"` // 프로젝트 시작
+	EndDate      string `json:"end_date"`   // 프로젝트 끝
+	UserID       int64  `json:"user_id"`
+	IsOnline     bool   `json:"is_online"`
+	CreatedAt    string `json:"created_at"`
+	UpdatedAt    string `json:"updated_at"`
 }
 
 func (s *StudyPost) Validate() map[string]string {
