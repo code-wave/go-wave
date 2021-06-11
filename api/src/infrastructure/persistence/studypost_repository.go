bc95977cd928e53ef50831e8e167825790aafba8
JungMin <zxaq15@gmail.com>
2021-06-11 19:46:45 +0900

    fix: Use Prepare before Query

--- api/src/infrastructure/persistence/studypost_repository.go
+++ api/src/infrastructure/persistence/studypost_repository.go
@@ -40,21 +40,19 @@ func (s *studyPostRepo) SavePost(studyPost *entity.StudyPost) (*entity.StudyPost
 }
 
 func (s *studyPostRepo) GetPost(id uint64) (*entity.StudyPost, error) {
-	row := s.db.QueryRow(`
+	stmt, err := s.db.Prepare(`
 		SELECT id, user_id, title, topic, content, num_of_members, is_mento, price, start_date, 
 		       end_date, is_online, created_at, updated_at
 		FROM study_post
 		WHERE id=$1;
-	`, id)
-
-	err := row.Err()
+	`)
 	if err != nil {
-		return nil, err // TODO: 나중에 에러처리 보완?
+		return nil, err // TODO: 나중에 보완
 	}
 
 	var studyPost entity.StudyPost
 
-	err = row.Scan(&studyPost.ID, &studyPost.UserID, &studyPost.Title, &studyPost.Topic, &studyPost.NumOfMembers,
+	err = stmt.QueryRow(id).Scan(&studyPost.ID, &studyPost.UserID, &studyPost.Title, &studyPost.Topic, &studyPost.NumOfMembers,
 		&studyPost.IsMento, &studyPost.Price, &studyPost.StartDate, &studyPost.EndDate, &studyPost.IsOnline,
 		&studyPost.CreatedAt, &studyPost.UpdatedAt)
 	if err != nil {
@@ -65,17 +63,21 @@ func (s *studyPostRepo) GetPost(id uint64) (*entity.StudyPost, error) {
 }
 
 func (s *studyPostRepo) GetPostsInLatestOrder(limit, offset uint64) (entity.StudyPosts, error) { // TODO: uint64 관련해서 js의 number는 64bit float형이라 데이터 받을때 string으로 받아야함
-	rows, err := s.db.Query(`
+	stmt, err := s.db.Prepare(`
 		SELECT id, user_id, title, topic, content, num_of_members, is_mento, price, start_date, 
 		       end_date, is_online, created_at, updated_at
 		FROM study_post
 		ORDER BY created_at DESC
 		LIMIT $1 OFFSET $2;
-	`, limit, offset)
+	`)
 	if err != nil {
 		return nil, err
 	}
-	defer rows.Close()
+
+	rows, err := stmt.Query(limit, offset)
+	if err != nil {
+		return nil, err
+	}
 
 	var studyPosts entity.StudyPosts
 
