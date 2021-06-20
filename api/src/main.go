de8298c96df3067deb4a565cfb002fd5b64cd842
atg0831 <atg0831@naver.com>
2021-06-20 23:09:32 +0900

    styled: Add comment and modify variable

--- api/src/main.go
+++ api/src/main.go
@@ -21,7 +21,7 @@ func main() {
 	}
 	defer services.Close()
 
-	authService, err := persistence.NewRedisDB(config.RedisHost, config.RedisPort, config.RedisPassword)
+	redisService, err := persistence.NewRedisDB(config.RedisHost, config.RedisPort, config.RedisPassword)
 	if err != nil {
 		log.Println(err)
 		return
@@ -30,18 +30,21 @@ func main() {
 	userApp := application.NewUserApp(services.User)
 	userHandler := interfaces.NewUserHandler(userApp)
 
-	authApp := application.NewAuthApp(authService.Auth)
+	authApp := application.NewAuthApp(redisService.Auth)
 	authHandler := interfaces.NewAuthHandler(userApp, authApp)
 	//interfaces.NewStudyPost(services.StudyPost)
 
 	r := chi.NewRouter()
+	//users
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("hello")) })
 	r.With(middleware.AuthVerifyMiddleware).Get("/users/{user_id}", userHandler.GetUser)
 	r.Get("/users/limit={limit}&offset={offset}", userHandler.GetAllUsers)
 	r.Post("/users/signup", userHandler.SaveUser)
-	r.Post("/auth/users/login", authHandler.LoginUser)
 	r.With(middleware.AuthVerifyMiddleware).Patch("/users/{user_id}", userHandler.UpdateUser)
 	r.With(middleware.AuthVerifyMiddleware).Delete("/users/{user_id}", userHandler.DeleteUser)
+
+	//auth
+	r.Post("/auth/users/login", authHandler.LoginUser)
 	r.With(middleware.AuthVerifyMiddleware).Post("/auth/users/logout", authHandler.LogoutUser)
 	r.With(middleware.AuthVerifyMiddleware).Post("/auth/users/refresh", authHandler.Refresh)
 
