6b64de5e0c790a5db0bcba3bde7337fa639873d1
Taegeon An <44899448+atg0831@users.noreply.github.com>
2022-01-08 01:49:01 +0900

    fix: Modify runserver.sh

--- runserver.sh
+++ runserver.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-if [[ "$1" = "--prod" ]]; then
-    echo "Run $2 server for production mode..."
-    docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build $2
+if [[ "$1" = "--local" ]]; then
+    echo "Run $2 server for local mode..."
+    docker-compose -f docker-compose.yml -f docker-compose.local.yml up -d --build $2
 elif [[ "$1" = "--dev" ]]; then
     echo "Run $2 server for develop mode..."
     docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build $2
